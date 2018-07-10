1bad5bfe297708e599290ae1e6e4530fd6f1c436
pengjiali <pengjiali@standard-robots.com>
2018-07-10 20:26:28 +0800

    Add:数据类型判断

---
+++ TypeDetermination.js
@@ -0,0 +1,9 @@
+let a = null
+console.log(a === null) // true
+console.log(typeof a) // object
+console.log(typeof a === 'null') // false
+
+let b = undefined
+console.log(b === undefined) // true
+console.log(typeof b) // undefined
+console.log(typeof b === 'undefined')   // true
