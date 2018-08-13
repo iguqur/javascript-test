692f2784a82e7933daaa44698e7b858b6766d3ac
pengjiali <pengjiali@standard-robots.com>
2018-08-13 10:31:32 +0800

    Add:Array & Object is equals function

--- ArrayTest.js
+++ ArrayTest.js
@@ -42,3 +42,41 @@ console.log(arr2)
     console.log(sub);   // Uint8Array [ 2, 3, 0 ]
     console.log(uint8)
 }
+
+(function () {
+    console.log('----------------------------数组是否相等判断------------------------------------------')
+    let a = [1, 2]
+    let b = [1, 2]
+
+    console.log(a === b) // result: => false // conclution:只是判断是否为同一个实例
+
+    // Warn if overriding existing method
+    if(Array.prototype.equals)
+        console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
+// attach the .equals method to Array's prototype to call it on any array
+    Array.prototype.equals = function (array) {
+        // if the other array is a falsy value, return
+        if (!array)
+            return false;
+        // compare lengths - can save a lot of time
+        if (this.length != array.length)
+            return false;
+        for (var i = 0, l = this.length; i < l; i++) {
+            // Check if we have nested arrays
+            if (this[i] instanceof Array && array[i] instanceof Array) {
+                // recurse into the nested arrays
+                if (!this[i].equals(array[i]))
+                    return false;
+            }
+            else if (this[i] != array[i]) {
+                // Warning - two different object instances will never be equal: {x:20} != {x:20}
+                return false;
+            }
+        }
+        return true;
+    }
+// Hide method from for-in loops
+    Object.defineProperty(Array.prototype, "equals", {enumerable: false});
+
+    console.log(a.equals(b)) // result: => true
+})()
--- ObjectTest.js
+++ ObjectTest.js
@@ -63,7 +63,7 @@
             let { commit } = this
 
             commit() // result => 'aaaaaaa'
-            this.__proto__.commit() // result => 'aaaaaaa'
+            this.__proto__.commit() // result => 'aaaaaaa' // conclution: 这样就可以解释为什么每个对象会有相同的成员函数了。（应为__proto__中的内容都是共享的）
         }
 
         commit () {
@@ -73,3 +73,66 @@
 
     let a = new A()
 });
+
+(function () {
+    console.log('----------------------------判断两个object是否相等------------------------------------------')
+    let a = {a: 1, b: 2}
+    let b = {a: 1, b: 2}
+
+    console.log(a === b) // result: => false // Object 的=== 操作只是判断两个Object是否是同一个实例
+
+    console.log(a.toString() === b.toString()) // result: => true // conclution: 可以将其转化成字符串再判断，但这样存在一些问问题
+
+    Object.prototype.equals = function(object2) {
+        //For the first loop, we only check for types
+        for (propName in this) {
+            //Check for inherited methods and properties - like .equals itself
+            //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/hasOwnProperty
+            //Return false if the return value is different
+            if (this.hasOwnProperty(propName) != object2.hasOwnProperty(propName)) {
+                return false;
+            }
+            //Check instance type
+            else if (typeof this[propName] != typeof object2[propName]) {
+                //Different types => not equal
+                return false;
+            }
+        }
+        //Now a deeper check using other objects property names
+        for(propName in object2) {
+            //We must check instances anyway, there may be a property that only exists in object2
+            //I wonder, if remembering the checked values from the first loop would be faster or not
+            if (this.hasOwnProperty(propName) != object2.hasOwnProperty(propName)) {
+                return false;
+            }
+            else if (typeof this[propName] != typeof object2[propName]) {
+                return false;
+            }
+            //If the property is inherited, do not check any more (it must be equa if both objects inherit it)
+            if(!this.hasOwnProperty(propName))
+                continue;
+            //Now the detail check and recursion
+            //This returns the script back to the array comparing
+            /**REQUIRES Array.equals**/
+            if (this[propName] instanceof Array && object2[propName] instanceof Array) {
+                // recurse into the nested arrays
+                if (!this[propName].equals(object2[propName]))
+                    return false;
+            }
+            else if (this[propName] instanceof Object && object2[propName] instanceof Object) {
+                // recurse into another objects
+                //console.log("Recursing to compare ", this[propName],"with",object2[propName], " both named \""+propName+"\"");
+                if (!this[propName].equals(object2[propName]))
+                    return false;
+            }
+            //Normal value comparison for strings and numbers
+            else if(this[propName] != object2[propName]) {
+                return false;
+            }
+        }
+        //If everything passed, let's say YES
+        return true;
+    }
+
+    console.log(a.equals(b)) // result: => true
+})()
