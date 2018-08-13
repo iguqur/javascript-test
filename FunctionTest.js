1285e5b69e45ab9ed97fc13c4f6d12bcd5308959
pengjiali <pengjiali@standard-robots.com>
2018-08-13 15:00:35 +0800

    Add: 匿名函数

--- FunctionTest.js
+++ FunctionTest.js
@@ -1,4 +1,4 @@
-{
+(function(){
     console.log('---------------------------------箭头函数是不会绑定函数、块作用于的上下文的------------------------------------------')
     function fun () {
     let a = 2
@@ -12,13 +12,29 @@
     }
 
 fun()
-}
+});
 
-{
+(function(){
     console.log('----------------------------------------------------------获取函数参数-------------------------------------------------------------------')
     let fun = (obj, str, num) => {
         console.log(arguments)
     }
 
     fun({a: 1, b: 2}, 'str', 333)
+});
+
+{
+    console.log('--------------------------------------------------匿名函数---------------------------------------------------------------------------')
+    let a = (function (a) {
+        console.log(a) // result: => 3
+        return 1
+    })(3)
+    console.log(a) // result: => 1
+
+    let obj = null
+    let fun = (function (arg) {
+        console.log(arg) // result: => {}
+    })(obj || {}) // 若object！==null，传入参数obj，否则传入参数{}
+
+    console.log(fun) // result: => undefined   (fun不是一个函数，而是函数返回的值）
 }
